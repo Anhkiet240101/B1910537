59b302ca3cf2a8eb4af71e2a1bcd43273b5eb1ac
Anhkiet2401 <kietb1910537@student.ctu.edu.vn>
2023-02-24 19:20:31 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const config = require("./app/config");
 
 // start server
-const PORT = config.app.PORT || 8080
+const PORT = config.app.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`Sever is running on http://localhost:${PORT}.`);
